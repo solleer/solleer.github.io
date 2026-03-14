fd76b9a15a34853e7e776ee6513739319aaa7ea3
Richard <solleer@hotmail.com>
2026-03-13 21:48:21 -0400

    Add top header

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,7 @@
 
 import litPlugin from '@lit-labs/eleventy-plugin-lit';
 import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
+import EleventyVitePlugin from "@11ty/eleventy-plugin-vite";
 
 export default function (eleventyConfig) {
     eleventyConfig.addPlugin(litPlugin, {
@@ -13,4 +14,10 @@ export default function (eleventyConfig) {
     eleventyConfig.addWatchTarget('js/');
     eleventyConfig.setInputDirectory("site");
     eleventyConfig.setOutputDirectory("dist");
+    eleventyConfig.addPassthroughCopy({
+        "node_modules/@awesome.me/webawesome/dist": "webawesome"
+    });
+    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("styles");
+    eleventyConfig.addPlugin(EleventyVitePlugin);
 };
--- site/index.md
+++ site/index.md
@@ -2,4 +2,8 @@
 title: Home
 ---
 
-This site is a work in progress.
+<wa-callout variant="brand" style="margin: var(--wa-space-xl)">
+  <wa-icon slot="icon" name="system/information-line" library="remixicon"></wa-icon>
+  <strong>This site is a work in progress.</strong><br/>
+  Please check back soon for more updates.
+</wa-callout>
