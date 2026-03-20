e707b24f512a651639ee9758d6363428118e0cf6
Richard <solleer@hotmail.com>
2026-03-20 12:28:32 -0400

    Fix ssr

--- scripts/ssr.ts
+++ scripts/ssr.ts
@@ -1,4 +1,2 @@
 // Import anything that you want server side rendered
 import './components/book-rating.ts';
-import '@awesome.me/webawesome/dist/components/scroller/scroller.js';
-import '@awesome.me/webawesome/dist/components/tab-group/tab-group.js';
