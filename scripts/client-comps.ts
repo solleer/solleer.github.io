7cd4dec8d93292af68ea1f8a7d9b571bb3ef8f56
Richard <solleer@hotmail.com>
2026-03-21 02:10:32 -0400

    Add education

--- scripts/client-comps.ts
+++ scripts/client-comps.ts
@@ -1,4 +1,5 @@
 import '@awesome.me/webawesome/dist/components/tab-group/tab-group.js';
 import '@awesome.me/webawesome/dist/components/callout/callout.js';
 import '@awesome.me/webawesome/dist/components/scroller/scroller.js';
+import '@awesome.me/webawesome/dist/components/details/details.js';
 import './components/icon-config';
